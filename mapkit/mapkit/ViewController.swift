69a19b2e65f2d95e8a4bf6b52645e04691d6572e
cayetanoledesma <92015135+cayetanoledesma@users.noreply.github.com>
2023-08-21 10:34:05 +0200

    Add files via upload

---
+++ mapkit/mapkit/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  mapkit
+//
+//  Created by Jesús Fernández on 21/2/23.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
